f4398c1cd0b44da0144d5cbd5339085ef91f85b0
Mauricio <maur.gomes@gmail.com>
2023-07-11 16:48:35 -0300

    fix: Add routes

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const router = jsonServer.router("api-mock.json");
 
 const middleware = jsonServer.defaults();
 server.use(middleware);
+
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1"
+  })
+)
 server.use(router);
 server.listen(3001, () => {
   console.log("JSON Server is running ...");
