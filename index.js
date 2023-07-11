89cf05d8764380d86ae2950f6745de8135b816db
Mauricio <maur.gomes@gmail.com>
2023-07-11 16:39:12 -0300

    feat: Add vercel config

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("api-mock.json");
+
+const middleware = jsonServer.defaults();
+server.use(middleware);
+server.use(router);
+server.listen(3001, () => {
+  console.log("JSON Server is running ...");
+})
+
+module.exports = server;
