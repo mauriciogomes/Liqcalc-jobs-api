db5abc2e87ca5c338c2008f7ab63a7f674cd9cf6
Mauricio <maur.gomes@gmail.com>
2023-07-17 18:51:24 -0300

    fix: move the index.js to api/ directory

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("api-mock.json");
 
-const middleware = jsonServer.defaults({"readOnly": "true"});
+const middleware = jsonServer.defaults();
 server.use(middleware);
 
 server.use(
@@ -12,12 +12,15 @@ server.use(
 )
 server.use(router);
 
+/*
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://liqcalc-jobs.vercel.app/')
-  res.header('Access-Control-Allow-Headers', '*')
-  res.header('Access-Control-Allow-Methods', 'GET')
+  // res.header('Access-Control-Allow-Origin', 'https://liqcalc-jobs.vercel.app/')
+  res.header('Access-Control-Allow-Origin', '*')
+  // res.header('Access-Control-Allow-Headers', '*')
+  // res.header('Access-Control-Allow-Methods', 'GET,OPTIONS')
   next()
 })
+*/
 
 server.listen(3000, () => {
   console.log("JSON Server is running ...");
